e8931ad60f5e04107f0ba455a319cf020f7f712f
Birhane Gebrial <57440782+Birhane-G@users.noreply.github.com>
2022-04-07 08:40:04 -0700

    c programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /*
+ * main 
+ *
  * This is simple c program to display som text using printf function
  *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
