082a76e2819f59850bee572cdb9c72447493cd43
Birhane Gebrial <57440782+Birhane-G@users.noreply.github.com>
2022-04-07 08:27:01 -0700

    c programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 	#include <stdio.h>
-       /*this is simple c program to display some text using puts function*/
+       /*
+	* main - Entry point
+	*
+	* This is simple c program to display some text using puts function
+	*
+	*/
 	int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
