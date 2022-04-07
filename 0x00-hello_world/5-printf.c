32165611a0696377b8e1613746eff92763352f8d
Birhane Gebrial <57440782+Birhane-G@users.noreply.github.com>
2022-04-07 09:15:13 -0700

    c programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - write "with proper grammar, but the outcome is a piece of art," 
+ * 
  * Return: Allways 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
