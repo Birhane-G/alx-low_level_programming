1d1927a5da2e45380167f13a9019df2bd5e4b310
Birhane Gebrial <57440782+Birhane-G@users.noreply.github.com>
2022-04-07 09:35:09 -0700

    c programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-* main - write '"Programming is like building a multilingual puzzle'
-* Return:Always 0 (success)
+* main - Entry point
+*
+* Return: Always 0 (success)
 */
 int main(void)
 {
