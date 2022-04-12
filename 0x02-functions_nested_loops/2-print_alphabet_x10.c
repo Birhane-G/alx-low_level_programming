441b4e400bc9b17e2236ed4bac7f01d983f7363f
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-12 19:59:27 +0300

    c programming language

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,16 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		char a = 'a';
-		while(a <= 'z')
+
+		while (a <= 'z')
 		{
+
 		putchar(a);
 		a++;
+
 		}
 		putchar('\n');
 	i++;
