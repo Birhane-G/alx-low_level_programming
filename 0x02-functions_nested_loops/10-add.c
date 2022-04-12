cebd360281398c36b93b80ddeec10d214a07b676
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-12 23:24:48 +0300

    c programming language

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,24 +1,18 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
+ * add - number
+ * @a: first arg
+ * @b: second arg
  *
- * main - check the code
- *
- * Return: Always 0.
+ * Return: addition 0
  *
  */
-
-int main(void)
-
+int add(int a, int b)
 {
+	int c;
 
-	    int n;
-
-	        n = add(89, 9);
-
-		    printf("%d\n", n);
-
-		        return (0);
-
+	c = a + b;
+	return (c);
 }
