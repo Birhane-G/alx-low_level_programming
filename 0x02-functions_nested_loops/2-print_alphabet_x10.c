f2f59323b57ced8bae7827775d2364dbe6a09b43
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-12 19:57:27 +0300

    c programming language

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * main - check the code
+ * print_alphabet_x10 - to print alphabet
  *
  * return: always 0
  */
 void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	while(i < 10)
 	{
 		char a = 'a';
