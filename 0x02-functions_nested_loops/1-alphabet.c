210f4adb37148de0d73fd0f78033f69e6acbd9b2
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-12 18:40:22 +0300

    c programming language

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  *
  * return: Always 0.
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 
 	char a = 'a';
-	 while(a <= 'z')
+	 while( a <= 'z' )
 	{
-	  _putchar(a);
-	  a++;
+	  	_putchar( a );
+	  	a++;
       	}
 	_putchar('\n');
 }
