4a31ea540bb710dd4baf56ae19bda96c4338eb49
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-14 21:18:13 +0300

    c programming language

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,19 +3,21 @@
 
 /**
  * print most numbers - print numbers 0 - 9
+ * it doesn`t print 2 and 4
  *
  * return: no return value
  */
+
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 59; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i == 50 || i == 53)
+		if (i == 50 || i == 52)
 		continue;
 
-		_putchar(i);
+	_putchar(i);
 	}
 	_putchar('\n');
 }
