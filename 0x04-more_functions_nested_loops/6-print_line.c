061334bda0a1622752227a96d95a648163c12186
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-15 00:22:10 +0300

    c programming language

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * Print_line - print straight line in terminal
@@ -15,11 +14,12 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
-		_putchar('_');	
+		_putchar('_');
+	}
 	else if ( n <= 0)
 	{
 	_putchar('\n');
