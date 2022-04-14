efa77a00f45125558eccb02fa32ae416f8d3f0fc
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-15 01:11:09 +0300

    c programming language

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - draw diagonal line on terminal
+ * @n: number of times should be print
+ *
+ * Return: no return value
+ */
+void print_diagonal(int n)
+{
+	int i;
+	if (n > 0)
+	{		
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
