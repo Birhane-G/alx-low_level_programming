5d08f34bf5ff230d8328994efe8d7e27da2b5822
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-15 02:07:16 +0300

    c programming language

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,19 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * Print_line - draws a straight line in the terminal
- * @n: number of times the line character should be printed
- *
- * Return: no return
+ * print_line - a function that draws a straight line in the terminal.
+ * followed by a new line.
+ * @n: An input integer
+ * Return: Always 0
  */
 void print_line(int n)
 {
-	int i;
+	int i = 0;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-		_putchar('_');
-		}
+		for (; i < n; i++)
+			_putchar('_');
+	}
 	_putchar('\n');
-	}
-	else if (n <= 0)
-	{
-		_putchar('\n');
-	}
 }
