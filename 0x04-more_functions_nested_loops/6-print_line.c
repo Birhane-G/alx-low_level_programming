60ff7f80d4e32387591268a34c106853c630b1c6
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-15 01:01:55 +0300

    c programming language

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 
 /**
  * Print_line - draws a straight line in the terminal
- * @n: number of times the line character should be  printed
+ * @n: number of times the line character should be printed
  *
  * Return: no return
  */
@@ -16,12 +16,12 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-		_putchar('_');
+			_putchar('_');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else if (n <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
