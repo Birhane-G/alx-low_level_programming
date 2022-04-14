2d3fa6a4f368326cf905c731dc82ba6725699480
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-14 20:15:09 +0300

    c programming language

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isdigit - check the code
+ * @c: para
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if(isdigit(c))
+		return (1);
+	else
+		return (0);
+}
