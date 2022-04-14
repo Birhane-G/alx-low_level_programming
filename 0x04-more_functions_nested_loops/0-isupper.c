a63244e81e3e473ce0d19727bbc4e81a2315120d
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-14 19:35:48 +0300

    c programming language

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check if the code is upper
+ * case or not 
+ *
+ * Return: 1 or 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
