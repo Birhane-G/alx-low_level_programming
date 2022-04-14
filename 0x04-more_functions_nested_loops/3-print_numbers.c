47f9d648744138de96e25ff9882110d081ce75dc
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-14 20:55:37 +0300

    c programming language

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_numbers - print numbers 0 - 9
  *
- *
  * Return: no Return value
  */
+
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 48; i < 58; i++)
 	{
-		_putchar("%c", i);
+	_putchar("%c", i);
 	}
 	_putchar('\n');
 }
