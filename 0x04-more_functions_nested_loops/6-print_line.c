37b13ba9f45e7a6d95439599dd7be2a0a9519663
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-15 00:53:23 +0300

    c programming language

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,12 +16,12 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('_');
+		_putchar('_');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
