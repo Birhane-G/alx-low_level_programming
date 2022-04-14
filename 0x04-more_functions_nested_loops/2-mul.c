03746c9e3c43f0ff7d7c7cc41be068a6eafae39c
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-14 20:34:30 +0300

    c programming language

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,15 @@
 
 /**
  * mul - multiplication of two number
+ * @a: multiplied with b
+ * @b: multiplied with a
  *
  * Return: sum
  */
 int mul(int a, int b)
 {
 	int sum;
+
 	sum = a * b;
 
 	return (sum);
