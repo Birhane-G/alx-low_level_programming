1e38843f031c625a98fa5ea851f381331a8e7ec3
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-20 13:28:33 +0300

    c programming language

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,22 +3,33 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - generates random passwords for 101-crackme
+ * main - generates random passwords for 101-crackm
  *
  * Return: zero
  */
 int main(void)
 {
-	int sum;
-	char c;
+	char k[200];
+	int num = 0;
+	int random = 0;
+	char *key = k;
 
 	srand(time(NULL));
-	while (sum <= 2645)
+
+	while (num < 2645)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		random = rand() % 122;
+
+		if (random > 32)
+		{
+			*key = random;
+			key = key + 1;
+			num += random;
+		}
 	}
-	putchar(2772 - sum);
+
+	*key = (2772 - num);
+	*(key + 1) = '\n';
+	printf("%s", k);
 	return (0);
 }
