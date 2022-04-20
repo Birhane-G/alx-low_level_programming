27ea1de8117de52e56487dffb7c6f350623ba038
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-20 18:20:13 +0300

    c programming language

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
  *
- *
- *
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
