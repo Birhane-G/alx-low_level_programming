6d41bf718e839c2c21a8e5571f92f054211e388c
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-04-28 01:00:25 +0300

    c programming language

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,24 @@
 #include <stdio.h>
 /**
  * _swrt_recursion - returns the natural square root of a number
+ * @n: to find sqrt
  *
- * Return 
+ * Return: natural square root or -1 
  */
 int _sqrt_recursion(int n)
 {
 	if(n == 0)
-		return (-1);
+		return (0);
 
 	return (check(1, n);
 }
 
 /**
  * check - check the square root of b
+ * @a: guess
+ * @b: number to find
  *
- * return -1 or b
+ * Return: 1 or b
  */
 
 int check(int a, int b)
