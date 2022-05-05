d5778fa404ffc0d0281221cd295ca418c9dc65f8
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-05-05 18:05:23 +0300

    c programming language

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@ int _strlen(char *s)
 {
 	unsigned int i;
 
-	i=0;
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
@@ -54,11 +54,10 @@ char *_strdup(char *str)
 	if (str == 0)
 	{
 		return (NULL);
-	
 	}
 
 	size = _strlen(str) + 1;
-	dst = (char *) malloc(size *sizeof(char));
+	dst = (char *) malloc(size * sizeof(char));
 
 	if (dst == 0)
 	{
