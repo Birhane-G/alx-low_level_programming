d50367f6cb836b4be2a229d1652d9c7df725d7b5
Birhane Gebrial <Birhanegebrial1@gmail.com>
2022-05-05 17:36:49 +0300

    c programming language

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_array - array for print a string
+ * @c: char
+ * @size: number of element
+ *
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	cahr *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size)
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		return (buffer)
+	}
+}
