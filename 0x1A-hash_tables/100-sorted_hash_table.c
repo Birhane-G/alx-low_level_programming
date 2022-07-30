782d4b68892c4656664859c0bce9337e1d607241
Birhane <Birhanegebrial1@gmail.com>
2022-07-30 16:59:05 +0300

    c programming

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -150,7 +150,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
  * shash_table_print - Prints a sorted hash table in order.
  * @ht: A pointer to the sorted hash table.
  */
-
 void shash_table_print(const shash_table_t *ht)
 {
     shash_node_t *node;
@@ -174,7 +173,6 @@ void shash_table_print(const shash_table_t *ht)
  * shash_table_print_rev - Prints a sorted hash table in reverse order.
  * @ht: A pointer to the sorted hash table to print.
  */
-
 void shash_table_print_rev(const shash_table_t *ht)
 {
     shash_node_t *node;
@@ -198,7 +196,6 @@ void shash_table_print_rev(const shash_table_t *ht)
  * shash_table_delete - Deletes a sorted hash table.
  * @ht: A pointer to the sorted hash table.
  */
-
 void shash_table_delete(shash_table_t *ht)
 {
     shash_table_t *head = ht;
